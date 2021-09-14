b9642d2a063ce04d500496587172ecfa5a27048c
molvqingtai <molvqingtai@gmail.com>
2021-09-14 19:24:16 +0800

    ci: initialize linter

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['standard', 'eslint-config-standard-with-typescript', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error'
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+const calc = (a: number, b: number): number => {
+  return a - b
+}
+console.log(calc(1024, 28))
